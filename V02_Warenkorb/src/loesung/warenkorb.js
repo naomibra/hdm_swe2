f028b932ac4d99068c1fbb160abafa98f72f65cf
Bräuing Naomi <nb108@hdm-stuttgart.de>
2020-04-28 17:15:41 +0200

    28.04.2020

---
+++ V02_Warenkorb/src/loesung/warenkorb.js
@@ -0,0 +1,49 @@
+let b1 = new Buch(
+    "Max Frisch",
+    "Andorra",
+    200,
+    9.99
+);
+let b2 = new Buch(
+    "Max Frisch",
+    "Homo Faber",
+    300,
+    12.99
+);
+let b3 = new Buch(
+    "Max Frisch",
+    "Stiller",
+    500,
+    19.99
+);
+let b4 = new Buch(
+    "Matin Suter",
+    "Elefant",
+    200,
+    9.99
+);
+let b5 = new Buch(
+    "Friedrich Dürrenmatt",
+    "Die Physiker",
+    250,
+    9.95
+);
+let b6 = new Buch(
+    "John Irving",
+    "Owen Meany",
+    800,
+    29.99
+);
+let b7 = new Buch(
+    "Martin Sutter",
+    "Small World",
+    350,
+    15.99
+);
+
+for(let i=0;i<Buch.lenght;i++){
+console,log(Buch[i].toString());
+
+}
+/*let s1 = b1.toString();
+console.log(s1);*/
