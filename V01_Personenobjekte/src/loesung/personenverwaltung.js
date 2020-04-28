d1ca65d20f7f21fde8fd78097ef987e3df6152b5
Bräuing Naomi <nb108@hdm-stuttgart.de>
2020-04-28 14:20:03 +0200

    28.04.2020

--- V01_Personenobjekte/src/loesung/personenverwaltung.js
+++ V01_Personenobjekte/src/loesung/personenverwaltung.js
@@ -11,4 +11,53 @@ let p3 =new Person();
 p3.name="Ute"
 p3.alter=75;
 
-console.log(p1);
+console.log(p1.toString());
+console.log(p2.toString());
+console.log(p3.toString());
+
+//ältere Person
+
+function aelterePerson(a, b){
+    if(a.alter>b.alter){
+        return a;
+    }
+    else if(b.alter>a.alter){
+        return b;
+    }
+    else if(a.alter===b.alter){
+        return b;
+    }
+    }
+
+//jüngere Person
+   function juengerePerson(a, b){
+    if(a.alter<b.alter){
+        return a;
+    }
+    else if(b.alter<a.alter){
+        return b;
+    }
+    else if(a.alter===b.alter){
+        return b;
+    }
+   }
+//Gleiche Persona?
+function sindGleich(a, b){
+if(a.equals(b)== true){
+return true;
+}
+return false;
+}
+
+//tests
+let test= aelterePerson(p1,p2);
+console.log(test.toString()+" ist die ältere Person");
+
+let test2 =juengerePerson(p2,p3);
+console.log(test2.toString()+" ist die jüngere Person")
+
+let test3 =sindGleich(p1,p1);
+console.log(test3);
+
+let test4 =sindGleich(p1,p2);
+console.log(test4);
