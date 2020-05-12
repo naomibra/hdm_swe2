dbb46771bdaefa71ecc7d6cf075806c4bea7246f
Bräuing Naomi <nb108@hdm-stuttgart.de>
2020-05-12 14:31:14 +0200

    12.05.2020

--- V07_Autorenliste/src/loesung/autorTest.js
+++ V07_Autorenliste/src/loesung/autorTest.js
@@ -10,10 +10,20 @@ let s3=a3.toString();
 let s4=a4.toString();
 //console.log(s1,s2,s3,s4);
 
+//zwischenfunktion true/false zu antwort
+function gleichOdernicht(autor1,autor2){
+    if(autor1.equals(autor2)==true){
+        return "gleich";
+    }
+    return "nicht gleich";
+}
+
 //equals
-console.log(s1+" und "+s2+"sind "+a1.equals(a2));
-console.log(s1+" und "+s3+"sind "+a1.equals(a3));
-console.log(s1+" und "+s4+"sind "+a1.equals(a4));
-console.log(s2+" und "+s3+"sind "+a2.equals(a3));
-console.log(s2+" und "+s4+"sind "+a2.equals(a4));
-console.log(s3+" und "+s4+"sind "+a3.equals(a4));
+console.log(s1+" und "+s2+"sind "+gleichOdernicht(a1,a2));
+console.log(s1+" und "+s3+"sind "+gleichOdernicht(a1,a3));
+console.log(s1+" und "+s4+"sind "+gleichOdernicht(a1,a4));
+console.log(s2+" und "+s3+"sind "+gleichOdernicht(a2,a3));
+console.log(s2+" und "+s4+"sind "+gleichOdernicht(a2,a4));
+console.log(s3+" und "+s4+"sind "+gleichOdernicht(a3,a4));
+
+
