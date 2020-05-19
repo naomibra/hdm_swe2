2df72ada6442d7678f1f3dfb03978797a4e2315f
Dr. Barbara Dörsam <doersam@hdm-stuttgart.de>
2020-05-19 17:43:34 +0200

    DOM-Korrekturen

--- V18_Json_Farben/src/loesung/farben.js
+++
@@ -1,9 +0,0 @@
-let farben = '{"konfig": {"farbe" : "rgb(0, 0, 255)", "schrift": "rgb(255, 0, 0)"}}';
-
-function farbenAendern() {
-    let konfig = JSON.parse(farben).konfig;
-    let body = document.getElementsByTagName("body")[0];
-    body.style.background = konfig.farbe;
-
-    body.style.color = konfig.schrift;
-}
