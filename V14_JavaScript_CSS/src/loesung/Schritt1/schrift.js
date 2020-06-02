d65dc7f3c2dbe04195508cab78f744858f698b7a
Bräuing Naomi <nb108@hdm-stuttgart.de>
2020-06-02 21:22:12 +0200

    02.06.2020

---
+++ V14_JavaScript_CSS/src/loesung/Schritt1/schrift.js
@@ -0,0 +1,27 @@
+//stronger-Element
+function stronger(selement){
+   selement.style.color="blue";
+   selement.style.fontStyle="italic";
+   selement.style.fontSize="50px";
+}
+//strong-Element normal 
+function snormal(kelement){
+    kelement.style.color="black";
+    kelement.style.fontSize="initial";
+    kelement.style.fontStyle="normal";
+}
+
+//em-Element
+function fett(emelment){
+    emelment.style.color="red";
+    emelment.style.fontSize="50px";
+    emelment.style.fontStyle="normal";
+    emelment.style.weight="normal";
+}
+
+//em-Element normal
+function emnormal(emneu){
+    emneu.style.fontSize="initial";
+    emneu.style.color="black";
+    emneu.style.fontStyle="italic";
+}
---
+++ V14_JavaScript_CSS/src/loesung/Schritt2/schrift.js
@@ -0,0 +1,13 @@
+//strong-element verändert
+function strong(selement){
+    selement.classList.add("toggle");
+}
+// normal
+function normal(daselement){
+    daselement.classList.remove("toggle");
+}
+
+//em-element verändert
+function em(emelement){
+    emelement.classList.add("toggle");
+}
