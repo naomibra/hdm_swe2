c825c4bea0b13091c07b18291a5cd97a40462bf8
Bräuing Naomi <nb108@hdm-stuttgart.de>
2020-06-02 16:47:39 +0200

    02.06.2020

--- V11a_Texte_und_Bilder/loesung/events.js
+++ V11a_Texte_und_Bilder/loesung/events.js
@@ -68,26 +68,3 @@
     }
 
  }
-//Bild 1 vergrößern
- function vergroessern(bild1){
-     bild1.style.width="80%";
- }
-//Bild 1 normal
-function normalzustand(bild1){
-bild1.style.width="30%";
-}
-
-//Bild 2 zoom
-function zoom(bild2){
-    bild2.style.width="150%";
-    bild2.style.top="25%";
-    bild2.style.botton="25%";
-    bild2.parentNode.style.overflow="hidden";
-}
-//Bild 2 normal
-function standard(bild2){
-    bild2.style.width="100%";
-    bild2.style.top="0%";
-    bild2.style.botton="0%";
-    bild2.parentNode.style.overflow="visible";
-}
